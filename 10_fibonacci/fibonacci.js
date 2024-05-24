0cfb3dfc029beb0f0af9dd6616a6ce833f5996b9
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-05-24 19:15:03 +0530

    Completed last excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = +position
+    if(position == 0){
+        return 0
+    }
+    if(position < 0){
+        return "OOPS"
+    }
+    if(position == 1){
+        return 1
+    }
+    if(position == 2){
+        return 1
+    }
+    return fibonacci(position -1) + fibonacci(position -2)
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let date = new Date()
+    return people.reduce((accumulator, item)=>{
+        //Define both together
+        let itemDeathYear = item.yearOfDeath;
+        let accumulatorDeathYear = accumulator.yearOfDeath;
+        if(item.yearOfDeath == undefined){
+            itemDeathYear = date.getFullYear()
+        }
+        if(accumulator.yearOfDeath == undefined){
+            accumulatorDeathYear = date.getFullYear()
+        }
+        let age_item = itemDeathYear - item.yearOfBirth
+        let age_accumulator = accumulatorDeathYear - accumulator.yearOfBirth
+        console.log(`age ${item.name}: ${age_item} age_accumulator: ${age_accumulator}` )
+        if( age_item > age_accumulator ){ //item's age - accumulator's age
+            accumulator = item
+        }
+        return accumulator
+    })
 };
 
 // Do not edit below this line
