33570a74ccb35efd2b48c05b0c09622eef17ba80
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-05-24 10:37:07 +0530

    Did 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    for (element of args) {
+        array = array.filter((num) => { if (element !== num) return true; else return false });
+    }
+    return array
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
